75b77f10fd8b11ae7716e3f0dc26be8e37579b7d
CruxChild <giovanethrb@gmail.com>
2021-12-03 12:03:32 -0300

    Scroll__Suave

--- script.js
+++ script.js
@@ -1,68 +1,96 @@
-const menuJs = document.querySelectorAll(".menuJs li a");
-const texto = [
-  ".sobre",
-  ".formação",
-  ".habilidade",
-  ".portifólio",
-  ".contato",
-  ".sobre",
-  ".formação",
-  ".habilidade",
-  ".portifólio",
-  ".contato",
-];
+function initExpandMenu() {
+  const menuJs = document.querySelectorAll(".menuJs li a");
+  const texto = [
+    ".sobre",
+    ".formação",
+    ".habilidade",
+    ".portifólio",
+    ".contato",
+    ".sobre",
+    ".formação",
+    ".habilidade",
+    ".portifólio",
+    ".contato",
+  ];
 
-const textoret = [
-  ".sbr",
-  ".frm",
-  ".hbl",
-  ".ptf",
-  ".cnt",
-  ".sbr",
-  ".frm",
-  ".hbl",
-  ".ptf",
-  ".cnt",
-];
+  const textoret = [
+    ".sbr",
+    ".frm",
+    ".hbl",
+    ".ptf",
+    ".cnt",
+    ".sbr",
+    ".frm",
+    ".hbl",
+    ".ptf",
+    ".cnt",
+  ];
 
-//Adicionando a classe inativo a todos os links dos menus
-menuJs.forEach((x) => {
-  x.classList.add("inativo");
-});
+  //Adicionando a classe inativo a todos os links dos menus
+  menuJs.forEach((x) => {
+    x.classList.add("inativo");
+  });
 
-//Transformando array-like menuJs em array
-menuJsA = Array.from(menuJs);
+  //Transformando array-like menuJs em array
+  menuJsA = Array.from(menuJs);
 
-//Primeira função de entrada do mouse no link
-function callback(event) {
-  event.preventDefault();
-  for (i = 0; i < menuJs.length; i++) {
-    if (menuJs[i].innerText === event.path[0].innerText) break;
+  //Primeira função de entrada do mouse no link
+  function callback(event) {
+    event.preventDefault();
+    for (i = 0; i < menuJs.length; i++) {
+      if (menuJs[i].innerText === event.path[0].innerText) break;
+    }
+
+    console.log((event.path[0].innerText = menuJs[i].innerText));
+    this.classList.toggle("ativo");
+    this.classList.toggle("inativo");
+    document.querySelector(".ativo").innerText = texto[i];
   }
 
-  console.log((event.path[0].innerText = menuJs[i].innerText));
-  this.classList.toggle("ativo");
-  this.classList.toggle("inativo");
-  document.querySelector(".ativo").innerText = texto[i];
-}
+  //Função de saida do mouse do link
 
-//Função de saida do mouse do link
+  function trueback(event) {
+    this.classList.toggle("ativo");
+    this.classList.toggle("inativo");
+    const inat = document.querySelectorAll(".inativo");
 
-function trueback(event) {
-  this.classList.toggle("ativo");
-  this.classList.toggle("inativo");
-  const inat = document.querySelectorAll(".inativo");
+    inat.forEach((x, y) => {
+      for (i = 0; i < 10; i++) {
+        x.innerText = textoret[y];
+      }
+    });
+  }
 
-  inat.forEach((x, y) => {
-    for (i = 0; i < 10; i++) {
-      x.innerText = textoret[y];
-    }
+  //chamada dos eventos
+
+  menuJs.forEach((event) => {
+    event.addEventListener("mouseover", callback);
+    event.addEventListener("mouseleave", trueback);
   });
 }
 
-//chamada dos eventos
+initExpandMenu();
 
-menuJs.forEach((event) => {
-  event.addEventListener("mouseover", callback);
-  event.addEventListener("mouseleave", trueback);
-});
+//scroll suave
+
+function initScroll() {
+  const menuJsHome = document.querySelectorAll(".menuJsHome a");
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    const topo = section.offsetTop;
+    console.log(topo);
+    window.scrollTo({
+      top: topo,
+      behavior: "smooth",
+    });
+  }
+
+  menuJsHome.forEach((item) => {
+    item.addEventListener("click", scrollToSection);
+  });
+}
+
+initScroll();
